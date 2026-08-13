6305053a0a872da3eaafff5ca46987fba572486d
Vivek Kumar <vivekkumar2011383@gmail.com>
2026-08-13 23:11:31 +0530

    Update resume link

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -56,7 +56,7 @@ export default function Hero() {
 
           <div className="flex flex-wrap justify-center gap-4">
             <a
-              href="https://drive.google.com/file/d/1b_CTlx6C5dcnR9yVvIMn9i1uhHBElDvz/view?usp=sharing"
+              href="https://drive.google.com/file/d/1SMnF30z5b5BsdSD3mCt2-zXQDDjdiFmL/view?usp=sharing"
               className="button-primary"
               target="_blank"
               rel="noopener noreferrer"
